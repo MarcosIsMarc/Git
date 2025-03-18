ffc4a2a09d7ba01b750cb832cc7b7149b4b99859
Marcos Arturo Aguilar <135053507+MarcosIsMarc@users.noreply.github.com>
2025-03-18 19:36:54 +0100

    Add files via upload

--- nostalgia/Escenas/player.gd
+++ nostalgia/Escenas/player.gd
@@ -3,58 +3,67 @@ extends CharacterBody2D
 @export var speed: float = 100.0
 @export var jump_force: float = -250.0
 @export var gravity: float = 900.0
-@export var run_speed: float = 125.0  # Velocidad al correr
-@export var run_jump_force: float = -275.0  # Salto al correr
+@export var run_speed: float = 125.0
+@export var run_jump_force: float = -275.0
 
-var facing_direction: int = 1  # 1 para derecha, -1 para izquierda
+var facing_direction: int = 1
 var attacking: bool = false
+var can_jump: bool = true
+var blocking: bool = false
 
-@onready var anim = $AnimatedSprite2D  # Usamos AnimatedSprite2D
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Aplicar gravedad
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Definir velocidad y salto según si Shift está presionado
 	var current_speed = speed
 	var current_jump_force = jump_force
 
-	if Input.is_action_pressed("move_shift"):  # Si Shift está presionado
+	if Input.is_action_pressed("move_shift"):
 		current_speed = run_speed
 		current_jump_force = run_jump_force
 
-	# Movimiento horizontal con A (derecha) y D (izquierda)
 	var direction = 0
-	if Input.is_action_pressed("move_right"):  # A -> Derecha
+	if Input.is_action_pressed("move_right"):
 		direction = 1
-	elif Input.is_action_pressed("move_left"):  # D -> Izquierda
+	elif Input.is_action_pressed("move_left"):
 		direction = -1
 
-	if direction != 0 and not attacking:
+	if direction != 0 and not attacking and not blocking:
 		velocity.x = direction * current_speed
 		facing_direction = direction
-		anim.flip_h = (facing_direction == -1)  # Voltea el sprite si mira a la izquierda
+		anim.flip_h = (facing_direction == -1)
 		anim.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
-		if is_on_floor() and not attacking:
+		if is_on_floor() and not attacking and not blocking:
 			anim.play("idle")
 
-	# Saltar con W
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor() and can_jump:
 		velocity.y = current_jump_force
 		anim.play("jump")
 
-	# Animación de caída
 	if velocity.y > 0 and not is_on_floor():
 		anim.play("fall")
 	
-	# Ataque con la flecha derecha
-	if Input.is_action_just_pressed("attack") and not attacking:
+	if Input.is_action_just_pressed("attack") and not attacking and is_on_floor():
 		attacking = true
+		can_jump = false
 		anim.play("attack")
-		await anim.animation_finished  # Espera a que termine el ataque
+		await anim.animation_finished
 		attacking = false
+		can_jump = true
+
+	if Input.is_action_pressed("block"):
+		blocking = true
+		anim.play("block")
+		if anim.frame >= 8:
+			anim.frame = 8
+			anim.stop()
+	else:
+		if blocking and anim.animation == "block" and anim.frame == 8:
+			anim.play()
+		blocking = false
 
 	move_and_slide()
